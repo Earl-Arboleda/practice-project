a0f97fd5c0d19d8cf6ca77e5c25c0d5fcfc18351
Earl-Arboleda <Earl.arboleda0000@gmail.com>
2023-05-27 20:12:28 +0800

    commit

---
+++ src/User.js
@@ -0,0 +1,16 @@
+const UserId = [{
+    id: "1",
+    img: "https://scontent.fdvo3-1.fna.fbcdn.net/v/t1.6435-9/47110656_1693108897460606_6463221878289858560_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEAFH6zrTg6fgpSglRc0SpTtlrw8R65Jv62WvDxHrkm_sn9x2xX2H98KfFVcPKOjLUAYUgDmFQ3YzGiLh-MMLM2&_nc_ohc=vctlaTg9QWkAX8s-qMy&_nc_ht=scontent.fdvo3-1.fna&oh=00_AfBV5z18fHrj0LGbffk0YmMf1_LjlhkNASShVXdDXoMRPQ&oe=6498F942",
+    name: "Earl C. Arboleda, Jr",
+    age: "23",
+    address: "Baldostamon blk 1, Koronadal City, South Cotabato",
+    details:"Cat lover"
+},{
+    id: "",
+    name: "",
+    age: "",
+    address: "",
+    details:""
+}]
+
+export default UserId;
